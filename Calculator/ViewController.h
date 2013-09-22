038778aa70b3a449090f570fbddb49a4d7785455
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-09-21 18:49:03 -0700

    Initial commit

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -9,5 +9,18 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int total;
+    int mode;
+    NSString *valueString;
+    IBOutlet UILabel *label;
+    BOOL lastButtonWasMode;
+}
 
+-(IBAction)tapedClear:(id)sender;
+-(IBAction)tapedNumber:(UIButton*)btn;
+-(IBAction)tapedPlus:(id)sender;
+-(IBAction)tapedMinus:(id)sender;
+-(IBAction)tapedMultiply:(id)sender;
+-(IBAction)tapedEquals:(id)sender;
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    valueString = @"";
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +27,74 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)tapedClear:(id)sender
+{
+    total = 0;
+    valueString = @"";
+    label.text = @"0";
+}
+-(IBAction)tapedNumber:(UIButton*)btn
+{
+    int num = btn.tag;
+    if (num == 0 && total == 0) {
+        return;
+    }
+    if (lastButtonWasMode) {
+        lastButtonWasMode = NO;
+        valueString = @"";
+    }
+    NSString *numAsString = [NSString stringWithFormat:@"%i", num];
+    valueString = [valueString stringByAppendingString:numAsString];
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber *n = [formatter numberFromString:valueString];
+    label.text = [formatter stringFromNumber:n];
+    if (total == 0) {
+        total = [valueString intValue];
+    }
+}
+-(IBAction)tapedPlus:(id)sender
+{
+    [self setMode:1];
+}
+-(IBAction)tapedMinus:(id)sender
+{
+    [self setMode:-1];
+}
+-(IBAction)tapedMultiply:(id)sender
+{
+    [self setMode:2];
+}
+-(IBAction)tapedEquals:(id)sender
+{
+    if (mode == 0) {
+        return;
+    }
+    if (mode == 1) {
+        total += [valueString intValue];
+    }
+    if (mode == -1) {
+        total -= [valueString intValue];
+    }
+    if (mode == 2) {
+        total *= [valueString intValue];
+    }
+    
+    valueString = [NSString stringWithFormat:@"%i", total];
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSNumber *n = [formatter numberFromString:valueString];
+    label.text = [formatter stringFromNumber:n];
+    mode = 0;
+}
+
+-(void)setMode:(int)m
+{
+    if (total == 0) {
+        return;
+    }
+    mode = m;
+    lastButtonWasMode = YES;
+    total = [valueString intValue];
+}
 @end
